4d57b45ee16bf3e9e9c3c88b00a966efff967cab
Karthika N. J <karthika.nj@gmail.com>
2022-12-21 11:13:32 +0530

    Update DeepLSTM.py

--- reef/lstm/DeepLSTM.py
+++ reef/lstm/DeepLSTM.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from keras.layers.embeddings import Embedding
+from keras.layers import Embedding
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
 from sklearn.linear_model import LogisticRegression
